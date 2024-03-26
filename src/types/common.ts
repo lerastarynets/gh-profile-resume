25563f4ce8114035fc412cdaa914b5cf7740d937
lera <starynetsv@gmail.com>
2024-03-26 12:34:19 +0200

    chore: add common types

---
+++ src/types/common.ts
@@ -0,0 +1,9 @@
+import { getUserRepos, getUser } from '../utils/api';
+
+type UserType = Awaited<ReturnType<typeof getUser>>['data'];
+
+type ReposType = Awaited<ReturnType<typeof getUserRepos>>['data'];
+
+export type RepoType = Awaited<ReturnType<typeof getUserRepos>>['data'][number];
+
+export type UserLoaderReturnType = { user: UserType; repos: ReposType };
