7730b09722408582996121161a2f8223fdf2fe67
lera <starynetsv@gmail.com>
2024-03-26 11:27:55 +0200

    chore: add mui, setup mui theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import  App from './App.tsx';
+import './index.css';
+import { createTheme, StyledEngineProvider, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </StyledEngineProvider>
+    </React.StrictMode>
+);
