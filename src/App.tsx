d248c9f774ae707fecff8cc449b73e0ff8206b95
lera <starynetsv@gmail.com>
2024-03-26 11:49:23 +0200

    feat: add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Params, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { getUser, getUserRepos } from './utils/api';
+import { CircularProgress } from '@mui/material';
 
-function App() {
-    const [count, setCount] = useState(0);
+const userLoader = async ({ params: { username } }: { params: Params<'username'> }) => {
+    const [user, repos] = await Promise.all([getUser(username!), getUserRepos(username!)]);
+    return { user: user.data, repos: repos.data };
+};
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <div />,
+    },
+    {
+        path: ':username',
+        element: <div />,
+        errorElement: <div />,
+        loader: userLoader,
+    },
+]);
+
+export const App = () => {
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        <main className="max-w-4xl p-6 flex flex-col space-y-5 items-center">
+            <RouterProvider fallbackElement={<CircularProgress />} router={router} />
+        </main>
     );
-}
-
-export default App;
+};
